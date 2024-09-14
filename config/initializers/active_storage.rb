e3db65bc941632e7e7bebe0d85bd1c31d35d16e1
Given Ncube <givenyslim12@gmail.com>
2024-09-14 06:43:36 +0200

    feat: added auth to active storage

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,8 @@
+Rails.configuration.to_prepare do
+  ActiveStorage::DirectUploadsController.class_eval do
+    before_action :authenticate_user!
+    rate_limit to: 20, within: 20.minutes, by: -> { current_user.id }
+  end
+end
+
 Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
